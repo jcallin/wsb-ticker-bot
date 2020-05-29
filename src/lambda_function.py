e7f29f8f9bc012a029fe2d15c092171e9caa380e
Julian Callin <j.callin@hotmail.com>
2020-05-28 21:19:04 -0700

    First version of bot with notifications and inbox receive enabled

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,6 +1,18 @@
-import os
-import logging
+from wsb_reddit import *
+
+logger = getLogger()
+
 
 def lambda_handler(event, context):
-    submissions = get_submissions()
+    logger.info(f"Running... {event}")
+    run()
+    logger.info("Finished Running")
 
+
+def run():
+
+    wsb_reddit = WSBReddit()
+    submissions = wsb_reddit.get_submissions()
+
+    wsb_reddit.process_inbox()
+    wsb_reddit.process_submissions(submissions)
--- tests/wsb_reddit_test.py
+++ tests/wsb_reddit_test.py
@@ -1,20 +1,46 @@
-import unittest
-
-from src.wsb_reddit import WSBReddit
+from fixtures import *
 from wsb_reddit_utils import get_tickers_for_submission
 
-
-class WSBRedditTest(unittest.TestCase):
-    def setUp(self):
-        self.wsb_reddit = WSBReddit()
-
-    def test_get_submissions(self):
-        self.wsb_reddit.get_submissions()
-
-    def test_get_tickers_for_submissions(self):
-        submissions = self.wsb_reddit.get_submissions()
-        [print((s.id, get_tickers_for_submission(s))) for s in submissions]
+logger = getLogger()
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.integration
+def test_get_submissions(wsb_reddit_client):
+    logger.info(wsb_reddit_client.get_submissions())
+
+
+@pytest.mark.integration
+def test_get_tickers_for_submissions(wsb_reddit_client):
+    submissions = wsb_reddit_client.get_submissions()
+    [logger.info((s.id, get_tickers_for_submission(s))) for s in submissions]
+
+
+@pytest.mark.integration
+def test_filter_nonexistent_tickers(wsb_reddit_client):
+    assert [t for t in ["asdf"] if wsb_reddit_client.database.ticker_exists(t)] == []
+    assert [t for t in ["$AAPL", "$ZM"] if wsb_reddit_client.database.ticker_exists(t)] == ["$AAPL", "$ZM"]
+
+
+@pytest.mark.integration
+def test_process_inbox(wsb_reddit_client):
+    wsb_reddit_client.process_inbox()
+
+
+@pytest.mark.integration
+def test_group_submissions_for_tickers(wsb_reddit_client, a_submission):
+    logger.info(wsb_reddit_client.group_submissions_for_tickers([a_submission], reprocess=True))
+
+
+@pytest.mark.integration
+def test_comment_on_submission(wsb_reddit_client, a_submission):
+    wsb_reddit_client.comment_on_submissions([a_submission], reprocess=True)
+
+
+@pytest.mark.integration
+def test_notify_users(wsb_reddit_client, a_submission):
+    wsb_reddit_client.notify_users([a_submission], reprocess=True)
+
+
+@pytest.mark.integration
+def test_process_rising_posts(wsb_reddit_client, a_submission):
+    wsb_reddit_client.process_submissions([a_submission])
